0372920b024152e5cab094bb809abc6891927d13
Varchas-401 <72722859+Varchas-401@users.noreply.github.com>
2021-09-02 19:41:32 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,9 @@
+class ATM(object):
+    def __init__(self,bankamazing):
+       self.bankamazing = bankamazing    
+
+    def withdraw(self):
+        print("withdrawing")
+
+    def bankenquiry(self):
+        print("Enter your enquiry")    
